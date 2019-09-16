445aedbac22beeae7605b84a0687890960b5341a
Bharat <yadavbharat163@gmail.com>
2019-09-17 00:40:21 +0530

    Updates

---
+++ precache-manifest.f36177d79112b69660ab48acdac5b0e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "284537f0a1f96a76b73a0810c3dd3f62",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "9a5779d9b83d8b50bde0",
+    "url": "/my-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "765edadd6fa764a48365",
+    "url": "/my-react-app/static/js/2.f6006a4c.chunk.js"
+  },
+  {
+    "revision": "9a5779d9b83d8b50bde0",
+    "url": "/my-react-app/static/js/main.f33e81fb.chunk.js"
+  },
+  {
+    "revision": "c9d7de98bdcba0fa772b",
+    "url": "/my-react-app/static/js/runtime~main.f754bbc5.js"
+  }
+]);
